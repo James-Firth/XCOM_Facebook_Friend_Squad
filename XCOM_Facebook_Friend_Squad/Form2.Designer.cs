547878a016cbeb63bcdf8d1a8411672a26545581
James Firth <hello@james-firth.com>
2012-10-20 02:10:48 -0500

    Logo and About Box
    Added the logo to the program and an about box. About Box currently
    unaccessible.

--- XCOM_Facebook_Friend_Squad/Form2.Designer.cs
+++ XCOM_Facebook_Friend_Squad/Form2.Designer.cs
@@ -33,12 +33,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.accessTokenTxtBox = new System.Windows.Forms.TextBox();
             this.authBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(87, 145);
+            this.linkLabel1.Location = new System.Drawing.Point(272, 462);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(107, 13);
             this.linkLabel1.TabIndex = 0;
@@ -49,7 +51,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(87, 9);
+            this.label1.Location = new System.Drawing.Point(200, 327);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(369, 117);
             this.label1.TabIndex = 1;
@@ -58,14 +60,14 @@
             // 
             // accessTokenTxtBox
             // 
-            this.accessTokenTxtBox.Location = new System.Drawing.Point(90, 180);
+            this.accessTokenTxtBox.Location = new System.Drawing.Point(60, 478);
             this.accessTokenTxtBox.Name = "accessTokenTxtBox";
-            this.accessTokenTxtBox.Size = new System.Drawing.Size(495, 20);
+            this.accessTokenTxtBox.Size = new System.Drawing.Size(603, 20);
             this.accessTokenTxtBox.TabIndex = 2;
             // 
             // authBtn
             // 
-            this.authBtn.Location = new System.Drawing.Point(90, 206);
+            this.authBtn.Location = new System.Drawing.Point(275, 514);
             this.authBtn.Name = "authBtn";
             this.authBtn.Size = new System.Drawing.Size(82, 23);
             this.authBtn.TabIndex = 3;
@@ -73,17 +75,28 @@
             this.authBtn.UseVisualStyleBackColor = true;
             this.authBtn.Click += new System.EventHandler(this.authBtn_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(60, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(603, 302);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(611, 373);
+            this.ClientSize = new System.Drawing.Size(738, 549);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.authBtn);
             this.Controls.Add(this.accessTokenTxtBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.linkLabel1);
             this.Name = "LoginForm";
             this.Text = "Authenticate!";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,5 +108,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox accessTokenTxtBox;
         private System.Windows.Forms.Button authBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
